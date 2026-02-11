252cebe6f432b8a333569937b4d383a1096fd454
Aziz-sehly <azizsehli068@gmail.com>
2026-02-11 20:49:01 +0100

    first commmit

---
+++ Microservice_proposal/src/main/java/com/esprit/microservice_proposal/MicroserviceProposalApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.microservice_proposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProposalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceProposalApplication.class, args);
+    }
+
+}
