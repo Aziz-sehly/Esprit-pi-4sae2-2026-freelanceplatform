5ff46286c49f90d23ed4a48d6c21ecb007e9cc36
HamzaGhorbel <hamza.ghorbel12345@gmail.com>
2026-02-12 11:24:17 +0100

    Add microservice project

---
+++ src/main/java/org/example/microservice_rn/MicroserviceRnApplication.java
@@ -0,0 +1,13 @@
+package org.example.microservice_rn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceRnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/microservice_rn/MicroserviceRnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microservice_rn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
