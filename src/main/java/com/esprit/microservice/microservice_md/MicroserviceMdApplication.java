13e87c015599f0a1d97961cd7f4ca8dbe46ec3b4
slimenmohamed <mohamedsadok.slimen@esprit.tn>
2026-02-12 11:36:57 +0100

    first push

---
+++ src/main/java/com/esprit/microservice/microservice_md/MicroserviceMdApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.microservice.microservice_md;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceMdApplication.class, args);
+    }
+
+}
